30740a010daab3aa6a358a6bc38701cdc216f10b
Camilo Valero Vazquez <62322797+CamilexXx@users.noreply.github.com>
2024-06-15 01:56:21 -0300

    Create snail_mail.py

---
+++ snail_mail.py
@@ -0,0 +1,6 @@
+# Write code below 
+print('-. Todays date: 15/06/24')
+print('-. How you are feeling right now: Im so exciting')
+print('-. What you want to accomplish by learning to code: I wannna be a TI Professional')
+print('-. A little message to your older, wiser, and programmer self: Keep practicing always')
+print('-. Your favorite emoji to spice things up!: Cool with sun glases face')
